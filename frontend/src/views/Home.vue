a26f1bf65595faa654aa20e987d49ad8f84e7984
Bkmx1220 <114698200+Bkmx1220@users.noreply.github.com>
2025-04-11 18:43:01 +0000

    Add frontend/src/views/Home.vue 1744396981503

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold text-gray-800 mb-4">Welcome to AgroConnect</h1>
+    <p class="text-gray-600">Farm management platform</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
